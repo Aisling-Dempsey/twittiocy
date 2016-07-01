289bfa3ee2710fe8a0e7499bbc33375d82cf20c1
Aisling Dempsey <aisling.n.dempsey@gmail.com>
2016-07-01 15:51:15 -0700

    Finished app. Needs tests and styling.

--- twitter-search.py
+++ twitter-search.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+from jinja2 import StrictUndefined
 import tweepy
-import os
-
+# import os
 
 app = Flask(__name__)
+
+# makes jinja throw error if requesting undefined value
+app.jinja_env.undefined = StrictUndefined
+
 
 # this is terrible practice and in reality, API keys should be sourced in through environmental vars or a similarly
 # secure process
@@ -12,6 +17,8 @@ consumer_key = 'bfMdk2m22f29bbo4EAVaREqTN'
 consumer_secret = 'FqQZQ7gXIO2UXuiSNsAfKEpXxpzPsrLMBulGRZTBj274Udm0ms'
 access_token = '748642641223385089-kqsibOsL2Wn0FgBoVzadW3ENMgdIPyB'
 access_token_secret = 'NvHHjCUGMqCIP8qbGQ54AK5SEVCBgM7ad8MmW4Upxh5OL'
+SECRET_KEY = 'q;qwisad90asdjek4ie9dd9#FASDFJ'
+
 
 # secure example, getting vars from a sourced secrets.sh
 #
@@ -19,15 +26,18 @@ access_token_secret = 'NvHHjCUGMqCIP8qbGQ54AK5SEVCBgM7ad8MmW4Upxh5OL'
 # consumer_secret = os.environ['CONSUMER_SECRET']
 # access_token = os.environ['ACCESS_TOKEN']
 # access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
+# SECRET_KEY = os.environ['SECRET_KEY']
 
+app.secret_key = SECRET_KEY
 
 @app.route('/')
 def splash():
-    return render_template('splash.html')
+    return render_template('base.html')
 
-@app.route('/results/<offset>')
+
+@app.route('/results/<int:offset>')
 def get_more_results(offset):
-    query = request.args.get('query')
+    query = request.args.get('term')
     tweet = get_tweet(query)
 
     # skips tweets that have been seen before. May be better/more efficient way to do this, but can't find
@@ -40,7 +50,8 @@ def get_more_results(offset):
                            tweets=tweets,
                            hashtags=hashtags,
                            final=final,
-                           offset=offset)
+                           offset=offset,
+                           query=query)
 
 
 
@@ -65,23 +76,40 @@ def build_tweets(t):
     tweets = []
     while len(tweets) < 25:
         try:
-            tweets.append(next(t))
+            tweet = next(t)
+            tweet_info = {
+                'created_at': tweet.created_at,
+                'hashtags': tweet.entities['hashtags'],
+                'text': tweet.text,
+                'name': tweet.user.name,
+                'screen_name': tweet.user.screen_name,
+                'fave_count': tweet.favorite_count
+            }
+            tweets.append(tweet_info)
         except StopIteration:
             break
     return tweets
+
+#     created_at --- datetime.datetime
+#     entities['hashtags'] --- array
+#     favorite_count ---  int
+#     text --- str
+#     user.name --- str
+#     user.screen_name --- str
+#     user.in_reply_to_screen_name ---str
 
 
 def make_hts(tweets):
     """takes result of query and builds list of all hashtags in result."""
     hts = []
     for tweet in tweets:
-        for ht in tweet.entities['hashtags']:
+        for ht in tweet['hashtags']:
             hts.append(ht['text'])
     return hts
 
 
 def sorted_hashtags(hts):
-    """takes hastag iterable and builds descending sorted list of hashtags and how often they appear."""
+    """takes hashtag array and builds descending sorted list of hashtags and how often they appear as tuples."""
     d = {}
     # builds dict of hashtags and their count
     for ht in hts:
@@ -90,7 +118,7 @@ def sorted_hashtags(hts):
     return sorted(d.items(), key=lambda k: k[1], reverse=True)
 
 
-def skip_to_place (offset, tweet):
+def skip_to_place(offset, tweet):
     """takes offset integer and skips appropriate number of tweets. Returns generator at appropriate place."""
     to_skip = (offset-1) * 25
     while to_skip > 0:
@@ -108,4 +136,10 @@ def last_page(tweets):
 
 
 if __name__ == '__main__':
+    # toggles debug mode on
+    app.debug = True
+
+    # runs debug toolbar
+    DebugToolbarExtension(app)
+
     app.run()
